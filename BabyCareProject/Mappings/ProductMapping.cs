db11434a231692dbccc9c1e9ece713a591105538
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-03-04 21:59:02 +0300

    Add project files.

---
+++ BabyCareProject/Mappings/ProductMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BabyCareProject.DataAccess.Entities;
+using BabyCareProject.Dtos.ProductDtos;
+
+namespace BabyCareProject.Mappings
+{
+    public class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ResultProductDto, Product>().ReverseMap();
+            CreateMap<CreateProductDto, Product>().ReverseMap();
+            CreateMap<UpdateProductDto, Product>().ReverseMap();
+        }
+    }
+}
